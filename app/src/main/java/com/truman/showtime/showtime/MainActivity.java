eca5b59bdb7a153af2d4804d705f0fb6a8c08be0
Chris Truman <cleetruman@gmail.com>
2015-01-18 23:31:04 -0800

    Rename weather stuff and initial attempt at theater display

--- app/src/main/java/com/truman/showtime/showtime/MainActivity.java
+++ app/src/main/java/com/truman/showtime/showtime/MainActivity.java
@@ -5,15 +5,13 @@ import android.os.Bundle;
 
 
 public class MainActivity extends Activity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new TheaterFragment())
                     .commit();
         }
     }
